f6332dd7f59d175283fe8daef99d7d7121366152
faizmalik1234 <49758890+faizmalik1234@users.noreply.github.com>
2019-04-18 20:39:11 +0530

    Add files via upload

---
+++ Secondprogram.py
@@ -0,0 +1,39 @@
+Python 3.7.3 (v3.7.3:ef4ec6ed12, Mar 25 2019, 21:26:53) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+================== RESTART: D:\code stuff]\FirstProgram.py ==================
+enter your name Faiz
+Hallo Faiz welcome to our shop
+enter your 1sr no : 5000
+enter your 2nd  no : 500
+enter you 3rd number : 50
+The sum of your three number is : 5550
+The sub of your three number is : 4450
+The mul of your three number is : 125000000
+The div of your three number is : 0.2
+>>> 
+================== RESTART: D:\code stuff]\FirstProgram.py ==================
+enter your name Zishan 
+Hallo Zishan  welcome to our shop
+enter your 1sr no : 12365000
+enter your 2nd  no : 20000
+enter you 3rd number : 320
+The sum of your three number is : 12385320
+The sub of your three number is : 12344680
+The mul of your three number is : 79136000000000
+The div of your three number is : 1.93203125
+Thnk you for calculating
+>>> 
+================== RESTART: D:\code stuff]\FirstProgram.py ==================
+enter your name Zishan
+Hallo Zishan welcome to our shop
+enter your 1sr no : 65412000000
+enter your 2nd  no : 456200
+enter you 3rd number : 6520
+The sum of your three number is : 65412462720
+The sub of your three number is : 65411537280
+The mul of your three number is : 194563022688000000000
+The div of your three number is : 21.991484737885386
+>>> 
+================== RESTART: D:\code stuff]\FirstProgram.py ==================
+enter your name 
